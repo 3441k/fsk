bb2b8df281aefe73a646f27656c03c8453544b36
3441k <65446990+3441k@users.noreply.github.com>
2022-03-21 00:59:42 +0400

    Final serializer and it's tb (tested via VCS)

--- modulation/modulation/verilogs/tb_serializer.v
+++ modulation/modulation/verilogs/tb_serializer.v
@@ -2,16 +2,30 @@
 `include "serializer.v"
 module tb();
 
-    reg clk;
-    wire [15:0] sin_out;
+    reg clk, load, rst;
+    reg [31:0] data;
+    wire Ren, out;
 
-    sinus_gen sin_wave(.clk(clk), .sinus(sin_out));
-    always #2 clk=~clk;
+    serializer serial(.clk(clk), .load(load), .rst(rst), .data(data), .Ren(Ren), .out(out));
+    always #1 clk=~clk;
 
     initial
 	begin
    	    clk = 1'b0;
-	    #3000 $finish;
+        rst = 1'b1;
+        
+        #2
+         load = 1'b1;
+         rst = 1'b0;
+         data = 32'b01100101110110010110101101100110        
+        
+        #40
+         data = 32'b00000000000111111111111111111100
+        
+        #30
+         load = 1'b1;
+         rst = 1'b0;
+	    #20 $finish;
 	end
 
 endmodule
